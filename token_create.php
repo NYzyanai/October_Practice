f77c54cf8a607e351585443d73b70f5f396ebfe4
NY <62198538+NYzyanai@users.noreply.github.com>
2021-10-07 18:20:57 +0900

    Update token_create.php

--- token_create.php
+++ token_create.php
@@ -24,24 +24,43 @@ function token()
 {
 
     //0~9,A~Zの入った配列を作る
-    //$num_alpha_array=array(0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+    $numAlphaArray=array("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
     //var_dump($num_alpha_array);
+
     //ランダムに取り出してトークンを作る
-    //$length_num_alpha_array=count($num_alpha_array);
-    //トークンの長さは40文字　
+    $token='';
+    $lengthOfToken=40;
 
-    $length_token=20;
 
-    //と思ったけど便利な関数があるらしいし安全なことが確認されている
 
-    $bytes=random_bytes($length_token);
-    $token=bin2hex($bytes);
 
-    /*random_bytesで20byte（160bit）のバイナリ文字列を返している。バイナリ文字列を16新数にbin2hexで戻すと、
-    00~ffまでの数字を得られる。
-    
-    だから20にしてるよ～
-    */
+    //array_rand使用
+    for($i=0;i<=$length_token;i++){
+        $token=$token.array_rand($numAlphaArray);
+    }
+
+
+
+
+    //もし自分でarray_randを作るとしたら？
+    //時間をキーにして関数作ったりしたら、逆に再現できる気がする…
+    for($i=0;i<=$length_token;i++){
+        //再現できてもよい？
+        $tokenBaseSeconds=date('ss');
+        $tokenBaseMinutes=date('ii');
+        //idと時間を加えたやつを取り出す
+        $randamTime=($id+$tokenBaseSeconds)*$tokenBaseMinutes;
+
+        //配列の全長
+        $numAlphaArrayLength=count($numAlphaArray)
+        //配列の全長で割って、その余り番目の配列の要素を取り出す
+        $takeArray=$randamTime%$numAlphaArrayLength
+        //tokenにくっつける
+        $token=$token.$numAlphaArray[$takeArray];
+    }
+
+
+
 
     //トークン部分を返す
     return $token;
